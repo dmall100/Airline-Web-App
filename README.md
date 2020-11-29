f7e3a48b16c8d1be79750974c8b826b7e306e5bd
Dylan <dylan.mall.dm@gmail.com>
2020-11-29 00:18:20 -0600

    git test

---
+++ README.md
@@ -0,0 +1,5 @@
+# Airline Web App 
+Web App using :
+ 1) client: html, css (style), js 
+ 2) server: (node) js
+ 3) database: using server using sql 
---
+++ client/book.js
@@ -0,0 +1,73 @@
+
+let flights = []
+let chosen_flight = [];
+const setflights= (data) => {
+  flights = data;
+}
+
+async function search() {
+  console.log("#searching")
+  // read the avflight description from input
+  //const origin = document.querySelector('#origin_city').value;
+  //const destination = document.querySelector('#destination_city').value;
+  //const connection = document.querySelector('#conn').value;
+  //const no_pass = document.querySelector('#pass_no').value;
+  //console.log(origin);-->
+
+  // use try... catch... to catch error
+  try {
+
+    // insert new avflight to "http://localhost:5000/avflights", with "POST" method
+    const response = await fetch("http://localhost:1385/flights");
+    const jsonData = await response.json();
+    console.log(jsonData)
+    for(var i = 0; i < 1;i++){
+       setflights(jsonData);
+       displayflights();
+    }
+
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+async function insert_pass() {
+  const name = document.querySelector("#pass-name").value;
+
+  try {
+    const body = {name: name}
+    const response = await fetch("http://localhost:1385/flights", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body)
+    });
+    location.reload();
+  } catch(err) {
+    console.log(err.message)
+  }
+}
+const displayflights = () => {
+  
+  const flightsTable = document.querySelector('#flights-table');
+
+  // display all todos by modifying the HTML in "todo-table"
+  let tableHTML = "";
+  flights.map(flight =>{
+    tableHTML +=
+    `<tr>
+    <th>${flight.departure_city}</th>
+    <th> $${flight.scheduled_departure}</th>
+    <th>${flight.arrival_city}</th>
+    <th> $${flight.scheduled_arrival}</th>
+    <th> <button onclick =  "chooseflight(${flight});location.href = 'costumer_info.html';"> select </button></th>
+    </tr>`;
+  })
+  flightsTable.innerHTML = tableHTML;
+}
+
+const chooseflight = (data) => {
+  chosen_flight = data;
+  const flight_info = document.querySelector('#flight-info');
+  flight_info.innerHTML =`<h3> ${chosen_flight.departure_city}</h3>` ;
+
+}
